6a1655f08fec1933257f114d0b844638e10740af
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2022-03-23 20:09:54 -0300

    Read and write from server to client

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use clap::{ArgEnum, Args, Parser};
 use local_ip_address::local_ip;
 use std::error::Error;
 use std::fmt;
+use std::io::{Read, Write};
 use std::net::TcpListener;
-use std::str::FromStr;
+use std::net::TcpStream;
+use std::str::{from_utf8, FromStr};
 
 #[derive(Debug, Clone)]
 pub struct ParseClientSideError;
@@ -69,7 +71,24 @@ fn main() -> std::io::Result<()> {
     let args = Cli::parse();
 
     match args.server_ip {
-        Some(ip) => {}
+        Some(ip) => {
+            // we are running a client
+            let mut stream = TcpStream::connect(format!("{}:4242", ip))?;
+            let msg = b"name=test";
+            stream.write(msg)?;
+
+            let mut data = [0 as u8; 50]; // using 50 byte buffer
+            loop {
+                match stream.read(&mut data) {
+                    Ok(size) => {
+                        if size > 0 {
+                            println!("received msg from server");
+                        }
+                    }
+                    Err(e) => println!("error listening to server"),
+                }
+            }
+        }
         None => {
             let local_ip = local_ip().unwrap();
 
@@ -84,8 +103,20 @@ fn main() -> std::io::Result<()> {
                 let listener = TcpListener::bind("0.0.0.0:4242")?;
 
                 for stream in listener.incoming() {
-                    println!("aaaaaa");
-                    println!("{:?}", stream);
+                    match stream {
+                        Ok(mut tcp_stream) => {
+                            let mut data = [0 as u8; 50]; // using 50 byte buffer
+                            tcp_stream.read(&mut data)?;
+                            let client_name =
+                                from_utf8(&data).unwrap().split('=').collect::<Vec<&str>>()[1];
+                            println!(
+                                "Client `{}` connected with addr `{}`",
+                                client_name,
+                                tcp_stream.peer_addr()?
+                            );
+                        }
+                        Err(e) => panic!(),
+                    }
                 }
             }
         }
