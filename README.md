b64da6d8d61c0db0e0188fad472d844494285017
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2022-04-20 00:11:50 -0300

    Add poc1 with simple solution

--- README.md
+++ README.md
@@ -18,3 +18,18 @@ cargo run -- --config config.toml
 cargo run -- --client client_name --server 192.0.0.1
 ```
 
+
+### POC 1
+
+Start server and client in the same machine. Config client to be at the left side
+of the server. When moving the mouse to the left of the server screen (0,y), the mouse
+goes to the right of the client screen (max_x-1, y) (both screens are the same in this case).
+Coming back with the mouse to the right of the client screen makes the mouse go back to the
+left side of the server (1, y).
+
+### Proposed implementation
+
+When the server starts, it starts a thread to monitor the mouse position. This thread constantly
+updates a Mouse struct with the new position, and checks if the mouse has reached a side
+that has a client.
+
--- src/core.rs
+++ src/core.rs
@@ -1,13 +1,29 @@
 use crate::cli::{ClientConfig, ClientSide, ServerConfig};
+use core_foundation::{
+    base::{kCFAllocatorDefault, ToVoid},
+    boolean::kCFBooleanTrue,
+    runloop::{kCFRunLoopCommonModes, CFRunLoop},
+    string::{kCFStringEncodingUTF8, CFStringCreateWithCString, CFStringRef},
+};
+use core_graphics::{
+    display::{CGDisplay, CGSSetConnectionProperty, _CGSDefaultConnection},
+    event::{
+        CGEventTap, CGEventTapLocation, CGEventTapOptions, CGEventTapPlacement, CGEventTapProxy,
+        CGEventType,
+    },
+    geometry::CGPoint,
+};
 use local_ip_address::local_ip;
 use std::io::{Read, Write};
 use std::net::IpAddr;
 use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::thread;
 
 pub struct Server {
     config: ServerConfig,
     addr: IpAddr,
-    listener: TcpListener,
+    // listener: TcpListener,
 }
 
 impl Server {
@@ -16,7 +32,7 @@ impl Server {
         Self {
             config,
             addr,
-            listener: TcpListener::bind(format!("{}:4242", addr)).unwrap(),
+            // listener: TcpListener::bind(format!("{}:4242", addr)).unwrap(),
         }
     }
 
@@ -25,27 +41,72 @@ impl Server {
         // improve this log
         println!("Server configuration: {:?}", self.config);
 
-        for stream in self.listener.incoming() {
-            match stream {
-                Ok(mut tcp_stream) => {
-                    let mut data = [0 as u8; 50]; // using 50 byte buffer
-                    tcp_stream.read(&mut data).expect("Failed to read tcp data");
-                    self.handle_msg(&data);
+        let (mouse_tx, mouse_rx) = mpsc::channel();
+
+        // mouse monitor thread
+        thread::spawn(move || {
+            dbg!("mouse thread spawned");
+            let cur_run_loop = CFRunLoop::get_current();
+            match CGEventTap::new(
+                CGEventTapLocation::HID,
+                CGEventTapPlacement::HeadInsertEventTap,
+                CGEventTapOptions::Default,
+                vec![CGEventType::MouseMoved /*CGEventType::LeftMouseDown*/],
+                |_a, _b, event| {
+                    mouse_tx.send(event.location()).unwrap();
+                    None
+                },
+            ) {
+                Ok(tap) => unsafe {
+                    let loop_source = tap.mach_port.create_runloop_source(0).unwrap();
+                    cur_run_loop.add_source(&loop_source, kCFRunLoopCommonModes);
+                    tap.enable();
+                    CFRunLoop::run_current();
+                },
+                Err(_) => panic!("event tap panicked"),
+            }
+        });
+
+        // TCP thread
+        let addr = self.addr;
+        thread::spawn(move || {
+            let listener = TcpListener::bind(format!("{}:4242", addr)).unwrap();
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(mut tcp_stream) => {
+                        let mut data = [0 as u8; 50]; // using 50 byte buffer
+                        tcp_stream.read(&mut data).expect("Failed to read tcp data");
+                        Server::handle_msg(&data);
+                    }
+                    Err(e) => panic!("Error in tcp stream"),
                 }
-                Err(e) => panic!("Error in tcp stream"),
+            }
+        });
+
+        let mut mouse_owner = "server";
+        for mouse_pos in mouse_rx {
+            // println!("mouse_pos: {}, {}", mouse_location.x, mouse_location.y);
+            if mouse_owner == "server" && mouse_pos.x == 0.0 {
+                mouse_owner = "client";
+                println!("the mouse is in {}", mouse_owner);
+                CGDisplay::warp_mouse_cursor_position(CGPoint::new(2558.0, mouse_pos.y)).unwrap();
+            } else if mouse_owner == "client" && mouse_pos.x > 2559.0 {
+                mouse_owner = "server";
+                println!("the mouse is in {}", mouse_owner);
+                CGDisplay::warp_mouse_cursor_position(CGPoint::new(1.0, mouse_pos.y)).unwrap();
             }
         }
     }
 
-    fn handle_msg(&self, msg: &[u8]) {
+    fn handle_msg(msg: &[u8]) {
         let decoded_msg = String::from_utf8(msg.to_vec()).expect("Failed to decode message");
         match decoded_msg.chars().collect::<Vec<char>>()[0] {
-            'c' => self.handle_first_conn(decoded_msg),
+            'c' => Server::handle_first_conn(decoded_msg),
             _ => println!("Unknow message"),
         }
     }
 
-    fn handle_first_conn(&self, msg: String) {
+    fn handle_first_conn(msg: String) {
         println!(
             "{} connected to the server",
             msg.split(' ').collect::<Vec<&str>>()[1]
